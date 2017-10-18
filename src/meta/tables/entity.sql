ead98f5c3f380479a46a320a9a9b8fba98fc3dba
Alex Campana <campana.alex@gmail.com>
2017-10-18 14:48:54 -0400

    Added seperate name length check for base abstract entity names. This will prevent the discriminator id from being truncated.

--- src/meta/tables/entity.sql
+++ src/meta/tables/entity.sql
@@ -3,4 +3,5 @@ CREATE TABLE meta.entity (
 ,   entity_type_code char(2) NOT NULL
 ,   CONSTRAINT pk_entity PRIMARY KEY (entity_name)
 ,   CONSTRAINT fk_entity__entity_type FOREIGN KEY (entity_type_code) REFERENCES meta.entity_type(entity_type_code)
+,   CONSTRAINT chk_entity__base_entity_name_length CHECK (entity_type_code <> 'ba' OR char_length(entity_name) <= 55)
 );
